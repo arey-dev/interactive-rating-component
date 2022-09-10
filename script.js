e972c42f6b24a8e288b44d5ec058d7f3e7e148f9
R.A Borja <raborja.dev@gmail.com>
2022-09-10 13:42:59 +0800

    refactor variables

--- script.js
+++ script.js
@@ -1,55 +1,38 @@
-function renderElements () {
+function renderState () {
   // get div container
   const container = document.querySelector('.container');
+  container.classList.toggle('card');
 
   // get radio button value
   const radioVal = getRate();
 
-  // create elements for thank you card
   const div = document.createElement('div');
-  const img = document.createElement('img');
-  const p_highlight = document.createElement('p');
-  const p_heading = document.createElement('p');
-  const p_message = document.createElement('p');
-
-  // add classes;
-  container.classList.toggle('card');
   div.className = 'img-wrapper';
-  p_highlight.className = 'highlight';
-  p_heading.className = 'heading';
-
-  //add attributes
+  
+  const img = document.createElement('img');
   img.src = './images/illustration-thank-you.svg';
   img.ariaHidden = 'true';
-
-  // add content to elements
-  p_highlight.textContent = 'You selected ' + radioVal + ' out of 5';
-  p_heading.textContent = 'Thank you!';
-  p_message.textContent = "We appreciate you taking the time to give a rating.If you ever need more support, don't hesitate to get in touch!";
-
-  // append elements
   div.append(img);
   container.append(div);
-  container.append(p_highlight);
+  
+  
+  const p_highlight = document.createElement('p');
+  p_highlight.className = 'highlight';
+
+  // if user the user picked a rate, show rate
+  if(radioVal) {
+    p_highlight.textContent = 'You selected ' + radioVal + ' out of 5';
+    container.append(p_highlight);
+  }
+  
+  const p_heading = document.createElement('p');
+  p_heading.className = 'heading';
+  p_heading.textContent = 'Thank you!';
   container.append(p_heading);
+  
+  const p_message = document.createElement('p');
+  p_message.textContent = "We appreciate you taking the time to give a rating.If you ever need more support, don't hesitate to get in touch!";
   container.append(p_message);
-}
-
-function handleChecked(wrapper, input) {
-  if(input.checked) {
-    wrapper.classList.remove('checked');
-    input.checked = false;
-    return;
-  }
-  // clear checks
-  for(let wrapper of wrappers) {
-    wrapper.classList.remove('checked');
-  }
-
-  if(!input.checked) {
-    wrapper.classList.add('checked');
-    input.checked = true;
-  }
 }
 
 function getRate() {
@@ -61,29 +44,47 @@ function getRate() {
 }
 
 const wrappers = document.querySelectorAll('.value-wrapper');
+function handleChecked(wrapper, radio) {
+
+  // condition to undo selection
+  if(radio.checked) {
+    wrapper.classList.remove('checked');
+    radio.checked = false;
+    return; // return immediately after deselecting 
+  }
+
+  // clear styles for all value-wrappers first
+  for(let wrapper of wrappers) {
+    wrapper.classList.remove('checked');
+  }
+
+  if(!radio.checked) {
+    wrapper.classList.add('checked');
+    radio.checked = true;
+  }
+}
+
 const radioDiv = document.querySelector('.radio-button-container');
-const btn = document.querySelector('input[type="submit"]');
-
-
-btn.addEventListener('click', function() {
-  // render 'thank you' card
-  renderElements();
-  
-  // remove form
-  const rating = document.forms.rating;
-  rating.remove();
-});
-
 radioDiv.addEventListener('click', function(event) {
+  event.preventDefault();
   if (event.target.className == 'radio-button-container' || event.target.tagName == 'LABEL') return;
 
   // get div wrapper
   const wrapper = event.target;
+  
+  // get radio input
+  const radio = wrapper.previousElementSibling;
 
-  // get input
-  const input = wrapper.previousElementSibling;
-  handleChecked(wrapper, input);
+  handleChecked(wrapper, radio);
+});
 
-  event.preventDefault();
+const submit = document.querySelector('input[type="submit"]');
+submit.addEventListener('click', function() {
+  // render 'thank you' card
+  renderState();
+  
+  // remove form
+  const rating = document.forms.rating;
+  rating.remove();
 });
 
